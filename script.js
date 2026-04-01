fde442cbbc5d25c17f09d32e5714e29b709b3dee
otaviobleich <ferreiraotavio577@gmail.com>
2026-04-01 10:59:38 -0300

    feat: adiciona contador de tarefas por coluna

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function atualizarContadores() {
+    document.getElementById("count-dev").innerText =
+        document.getElementById("dev").children.length;
+
+    document.getElementById("count-teste").innerText =
+        document.getElementById("teste").children.length;
+
+    document.getElementById("count-aprovado").innerText =
+        document.getElementById("aprovado").children.length;
+}
+
 function salvarDados() {
     const dados = {
         dev: document.getElementById("dev").innerHTML,
@@ -5,6 +16,7 @@ function salvarDados() {
         aprovado: document.getElementById("aprovado").innerHTML
     };
     localStorage.setItem("kanban", JSON.stringify(dados));
+    atualizarContadores();
 }
 
 function carregarDados() {
@@ -15,6 +27,8 @@ function carregarDados() {
         document.getElementById("teste").innerHTML = dados.teste;
         document.getElementById("aprovado").innerHTML = dados.aprovado;
     }
+
+    atualizarContadores();
 }
 
 window.onload = carregarDados;
